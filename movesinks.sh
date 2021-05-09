5460ad352944c60f8f85b99987b06a9ae91218c7
AJO-python@T580 <AJO-python@T580>
2021-05-09 17:01:56 +0100

    add user input to continue cycking sinks

--- movesinks.sh
+++ movesinks.sh
@@ -2,13 +2,19 @@
 # modify /etc/pulse/default.pa
 # > load-module module-stream-restore restore_device=false
 
-echo "Setting default sink to: $1";
-pacmd set-default-sink $1;
-pacmd list-sink-inputs | grep index | while read line
+i=0
+while true;
 do
-    echo "Moving input: ";
-    echo $line | cut -f2 -d' ';
-    echo "to sink: $1";
-    pacmd move-sink-input `echo $line | cut -f2 -d' '` $1;
+    echo "Setting default sink to: $i";
+    pacmd set-default-sink $i;
+    pacmd list-sink-inputs | grep index | while read line
+    do
+        echo "Moving input: ";
+        echo $line | cut -f2 -d' ';
+        echo "to sink: $i";
+        pacmd move-sink-input `echo $line | cut -f2 -d' '` $i;
+    done;
+    read -p "{CR} to continue, ^c to quit" 
+    i=$(expr $i + 1);
 done;
 
