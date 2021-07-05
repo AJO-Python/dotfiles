68b2198dcc81ce8844eb7283cef1f2caf9c15684
Josh-work <josh.owen121@gmail.com>
2021-07-05 10:52:52 +0100

    Fix order issue in elif with removing/unlinking old dotfiles. Switched to using $HOME for portability. Used "" to allow filenames with spaces

--- setup.sh
+++ setup.sh
@@ -8,9 +8,9 @@ FILES=(
     ".tmux.conf.local"
     )
 
-old_path="$HOME/dotfiles/old_dots";
+old_path="${HOME}/dotfiles/old_dots";
 
-if ! [ -d "$old_path" ]; then
+if [ ! -d "$old_path" ]; then
     echo "Making $old_path"
     mkdir "$old_path";
 fi;
@@ -18,10 +18,14 @@ fi;
 for file in ${FILES[*]}; do
     echo "On $file";
     # Moves existing files - leaves symlinks in place
-    if [ -f "$file" ]; then
-        mv $file $HOME/dotfiles/old_dots/ && echo "Moved $file to old_dots";
+    if [ -h "${HOME}/${file}" ]; then
+        echo "${HOME}/${file} is a symlink. Unlinking";
+        unlink "${HOME}/${file}"
+    elif [ -f "${HOME}/${file}" ]; then
+        echo "$file is a regular file in $HOME. Moving to $old_path";
+        mv "${HOME}/${file}" ${HOME}/dotfiles/old_dots/ && echo "Moved ${file} to old_dots";
     fi;
-    echo "symlinking $file to $HOME";
-    ln -sv $HOME/dotfiles/$file $HOME/$file
+    echo "symlinking ${HOME}/dotfiles/${file} to ${HOME}/${file}";
+    ln -sv "${HOME}/dotfiles/$file" "${HOME}/$file";
 done;
 echo "Done";
